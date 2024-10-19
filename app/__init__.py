0c5dfa5026ed5898b35c9565981a8d2051f54cde
gomesluiz <gomes.luiz@gmail.com>
2024-10-18 22:50:46 -0300

    feat: Add login functionality and configuration setup. Resolves: #3

--- app/__init__.py
+++ app/__init__.py
@@ -1,5 +1,9 @@
+import os
+
 from flask import Flask
+from app.config import Config
 
 app = Flask(__name__)
+app.config.from_object(Config)
 
 from app import routes
--- app/routes.py
+++ app/routes.py
@@ -1,17 +1,48 @@
-import os
-
-from flask import render_template
-from flask_sqlalchemy import SQLAlchemy
+from flask import render_template, flash, redirect, url_for
 from app import app
+from app.forms import LoginForm
+
+
+@app.route("/login", methods=["GET", "POST"])
+def login():
+    """
+    Render the login page with the login form.
+
+    Returns:
+        Response: The HTML content for the login page with the title 'Sign In'.
+    """
+    form = LoginForm()
+    if form.validate_on_submit():
+        flash(
+            f"Login requested for user {form.username.data}, remember_me={form.remember_me.data}"
+        )
+        return redirect(url_for("home"))
+    return render_template("login.html", title="Sign In", form=form)
 
 
 @app.route("/")
 @app.route("/home")
 def home():
+    """
+    Renders the home page with user information and a list of posts.
+
+    Returns:
+        str: The rendered HTML content for the home page.
+    """
     user = {"username": "Armin"}
     posts = [
         {"author": {"username": "John"}, "body": "Beautiful in Portland"},
         {"author": {"username": "Susan"}, "body": "The Avengers movie was so cool"},
     ]
-
     return render_template("home.html", title="Home", user=user, posts=posts)
+
+
+@app.route("/about")
+def about():
+    """
+    Render the 'about' page.
+
+    Returns:
+        Response: The HTML content for the 'about' page with the title 'About'.
+    """
+    return render_template("about.html", title="About")
